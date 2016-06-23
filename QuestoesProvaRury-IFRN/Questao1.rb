f615009e5fab76496ae23458237995396785b6a0
andersonpablo <mib_anderson@hotmail.com>
2016-06-23 14:03:52 -0300

    Update Questao1.rb

--- QuestoesProvaRury-IFRN/Questao1.rb
+++ QuestoesProvaRury-IFRN/Questao1.rb
@@ -1,10 +1,9 @@
+#Crie um script em Ruby que l√™ o sal√°rio de um funcion√°rio, reajusta o sal√°rio em 7% e mostra o resultado.
 
-Crie um script em Ruby que lÍ o sal·rio de um funcion·rio, reajusta o sal·rio em 7% e mostra o resultado.
-
-puts "digite o valor do sal·rio"
+puts "digite o valor do sal√°rio"
 salario = gets.chomp.to_f
 
 ajuste = salario * 0.07
 
-puts "o sal·rio com 7% de ajuste È: #{ajuste}"
+puts "o sal√°rio com 7% de ajuste √©: #{ajuste}"
 
