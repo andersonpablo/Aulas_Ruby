de73b3d913baec66fecaa714e1accc05de03500c
Anderson Pablo <mib_anderson@hotmail.com>
2016-07-11 15:28:38 -0300

    Array

---
+++ Atividades/array.rb
@@ -0,0 +1,18 @@
+=begin
+Iterando arrays
+=end
+
+# Forma 1
+
+meu_array = [10, 20, 30, 40, 50]
+meu_array.each { |elemento|
+ puts elemento
+}
+
+# Forma 2
+
+meu_array = [10, 20, 30, 40, 50]
+for elemento in meu_array
+ puts elemento
+end
+
