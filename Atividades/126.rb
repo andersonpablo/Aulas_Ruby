2443c2a1c87899ec65b37f2eff3a62c04b669be1
Anderson Pablo <mib_anderson@hotmail.com>
2016-07-22 20:08:41 -0300

    questão finalizada

--- Atividades/126.rb
+++ Atividades/126.rb
@@ -1,3 +1,5 @@
+# Enunciado da Questão
+
 =begin
 
 Crie um script em Ruby que leia o nome e a idade de várias pessoas. O script deve parar de ler nomes e idades quando o usuário teclar ENTER sem digitar nada para nome e idade. Por fim, o script deve calcular e mostrar:
@@ -10,34 +12,61 @@ A média das idades.
 
 =end
 
-nomeIdade = Hash.new
+# Váriáveis
 
+nomeIdade = Hash.new
 enter = 0
 calcIdade = 0
+entradas = 0
+# Entradas
 
 begin
 
 puts "Para sair tecle ENTER ou qualquer tecla para continuar"
 enter = gets.chomp
 
+# Verificação se a entrada é vazia
+
 	if enter == "" 
+
 		puts "Você teclou ENTER e saiu do programa"
+
 	else 
+
 		puts "Digite um nome"
 		nome = gets.chomp
 		puts "Digite uma idade"
 		idade = gets.chomp.to_i
+
+# Criando a Hash Nome => Valor
+
 		nomeIdade[nome] = idade
+		
+# Somanando número de entradas
+		
+		entradas = (entradas + 1)
+
+# Somatório das Idades
 
 		calcIdade = (calcIdade + idade)
+
+# Fim do IF
+
 	end
+
+# Enquanto diferente de vázio, continue!
 
 end while (enter != "")
 
+# AC e ACC = account = contador
 ac = 0
 acc = 0
 
+# Iterando nome, valor
+
 nomeIdade.each do |nome, idade|
+
+# Verificando idade e incrementando
 
  if idade >= 18
  	ac = (ac + 1)
@@ -45,10 +74,22 @@ nomeIdade.each do |nome, idade|
  	acc = (acc + 1)
  else 
  	puts "idade inválida"
+# Fim do IF
  end
+# Fim do Iterator
 end
-  media = (calcIdade / (ac + acc))
+
+# Calculando média das idades
+media = (calcIdade / (ac + acc))
+
+# Ordenando
+nomeIdade = nomeIdade.sort_by { |nome, idade| idade }
+
+# Saídas
+
+  puts "A pessoa mais velha é #{nomeIdade[entradas-1]}"
+  puts "A pessoa mais jovem é #{nomeIdade[0]}"
+  puts "ordenação: #{nomeIdade}"
   puts "maior que 18 anos: #{ac}"
   puts "menor que 18 anos: #{acc}"
   puts "media das idades: #{media}"
-  #puts "#{nome} tem #{idade} anos"
