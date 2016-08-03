22e8712b97030759baaeef93629ef3f603e569ad
Anderson Pablo <mib_anderson@hotmail.com>
2016-08-03 13:31:32 -0300

    atualização

---
+++ Atividades/142.rb
@@ -0,0 +1,14 @@
+=begin
+Na classe Pessoa, crie um método chamado falar que recebe um valor como parâmetro e mostre esse valor na tela.
+=end
+
+
+class Pessoa 
+
+	anderson = Pessoa.new
+
+	def falar(fala)
+		puts "Você falou: #{fala}"
+	end
+
+end
